5b51c68bddd44a7fd8cf18450fd3cb3432d9f799
JammieCoder <jammiedodger188@gmail.com>
2024-07-15 20:29:44 +0100

    Weeks seeded

--- database/seeders/WeekSeeder.php
+++ database/seeders/WeekSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Week;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class WeekSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Week::factory(12)->create();
     }
 }
