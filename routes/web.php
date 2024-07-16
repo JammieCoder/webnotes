4513d97bcc01004e8beaf7920ae191ef53ee27fb
JammieCoder <jammiedodger188@gmail.com>
2024-07-16 17:45:59 +0100

    Update for pages deployment

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'dashboard')->name('dashboard');
 Route::view('/login','auth.login')->name('login');
+Route::view('/modules/{module}','dashboard');
