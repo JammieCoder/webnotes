ef647520ee764f9634ce74ddbbc043374c4a2608
JammieCoder <jammiedodger188@gmail.com>
2024-07-25 13:58:19 +0100

    Implemented observer for the creation of a Note to make its file

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,17 +2,21 @@
 
 namespace App\Models;
 
+use App\Observers\NoteObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Facades\Storage;
+
+#[ObservedBy([NoteObserver::class])]
 
 class Note extends Model
 {
     use HasFactory;
 
     public function content(){
-        return Storage::disk('local')->get($this->filename);
+        return Storage::get($this->filename);
     }
 
     public function topics(): BelongsToMany{
---
+++ app/Observers/NoteObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Note;
+use Illuminate\Support\Facades\Storage;
+
+class NoteObserver
+{
+    /**
+     * Handle the Note "created" event.
+     */
+    public function created(Note $note): void
+    {
+        Storage::put($note->filename,"");
+    }
+
+    /**
+     * Handle the Note "updated" event.
+     */
+    public function updated(Note $note): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Note "deleted" event.
+     */
+    public function deleted(Note $note): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Note "restored" event.
+     */
+    public function restored(Note $note): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Note "force deleted" event.
+     */
+    public function forceDeleted(Note $note): void
+    {
+        //
+    }
+}
